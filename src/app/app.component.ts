a9dad2889c4c6ade06204d652fac31879951ece4
Julian Jandl <julian@jandl.wien>
2024-07-08 12:18:57 +0200

    feat: implement solution 5: Control Flow - For

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,41 +8,61 @@ import { AppShellComponent } from './app-shell/app-shell.component';
   imports: [AppShellComponent],
   template: `
     <app-shell>
-      <div class="movie-card">
-        <img
-          class="movie-image"
-          [alt]="movie.title"
-          [src]="'https://image.tmdb.org/t/p/w342' + movie.poster_path" />
-        <div class="movie-card-content">
-          <div class="movie-card-title">{{ movie.title }}</div>
-          <div class="movie-card-rating">{{ movie.vote_average }}</div>
+      @for (movie of movies; track movie.id) {
+        <div class="movie-card">
+          <img
+            class="movie-image"
+            [alt]="movie.title"
+            [src]="'https://image.tmdb.org/t/p/w342' + movie.poster_path" />
+          <div class="movie-card-content">
+            <div class="movie-card-title">{{ movie.title }}</div>
+            <div class="movie-card-rating">{{ movie.vote_average }}</div>
+          </div>
+          <button
+            class="favorite-indicator"
+            [class.is-favorite]="favoriteMovieIds.has(movie.id)"
+            (click)="toggleFavorite(movie)">
+            @if (favoriteMovieIds.has(movie.id)) {
+              I like it
+            } @else {
+              Please like me
+            }
+          </button>
         </div>
-        <button
-          class="favorite-indicator"
-          [class.is-favorite]="isFavorite"
-          (click)="toggleFavorite(movie)">
-          @if (isFavorite) {
-            I like it
-          } @else {
-            Please like me
-          }
-        </button>
-      </div>
+      }
     </app-shell>
   `,
 })
 export class AppComponent {
-  movie = {
-    id: 'the-god',
-    title: 'The Godfather',
-    vote_average: 10,
-    poster_path: '/3bhkrj58Vtu7enYsRolD1fZdja1.jpg',
-  };
+  movies = [
+    {
+      id: 'the-god',
+      title: 'The Godfather',
+      poster_path: '/3bhkrj58Vtu7enYsRolD1fZdja1.jpg',
+      vote_average: 10,
+    },
+    {
+      id: 'the-god-2',
+      title: 'The Godfather part II',
+      poster_path: '/hek3koDUyRQk7FIhPXsa6mT2Zc3.jpg',
+      vote_average: 9,
+    },
+    {
+      id: 'the-god-3',
+      title: 'The Godfather part III',
+      poster_path: '/lm3pQ2QoQ16pextRsmnUbG2onES.jpg',
+      vote_average: 10,
+    },
+  ];
 
-  isFavorite = false;
+  favoriteMovieIds = new Set();
 
   toggleFavorite(movie) {
-    this.isFavorite = !this.isFavorite;
     console.log('toggled', movie);
+    if (this.favoriteMovieIds.has(movie.id)) {
+      this.favoriteMovieIds.delete(movie.id);
+    } else {
+      this.favoriteMovieIds.add(movie.id);
+    }
   }
 }
